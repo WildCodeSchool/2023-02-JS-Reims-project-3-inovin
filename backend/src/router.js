79e4a14d9b1eb9e8303a31bf663fa6cad9b38ad4
Mohamed4900 <Mohamedibnlakhdar10@gmail.com>
2023-05-31 17:06:18 +0200

    update: table 'Wine' ajouté

--- backend/src/router.js
+++ backend/src/router.js
@@ -4,10 +4,18 @@ const router = express.Router();
 
 const userControllers = require("./controllers/userControllers");
 
+const wineControllers = require("./controllers/wineControllers");
+
 router.get("/users", userControllers.browse);
 router.get("/users/:id", userControllers.read);
 router.put("/users/:id", userControllers.edit);
 router.post("/users", userControllers.add);
 router.delete("/users/:id", userControllers.destroy);
+
+router.get("/wines", wineControllers.browse);
+router.get("/wines/:id", wineControllers.read);
+router.put("/wines/:id", wineControllers.edit);
+router.post("/wines", wineControllers.add);
+router.delete("/wines/:id", wineControllers.destroy);
 
 module.exports = router;
