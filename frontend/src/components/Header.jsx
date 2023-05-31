5114aa831f565f57b02dde4e1c93e8075c8384bc
Célia Kleczynki <Celia.kleczynski@hotmail.com>
2023-05-31 15:02:56 +0200

    Adding component Header.jsx

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,10 @@
+import Logo from "../Pictures/Logo.jpg";
+
+export default function Header() {
+  return (
+    <nav className="header">
+      <img src={Logo} alt="Logo Ino'vin" />
+      <h1>INO'VIN</h1>
+    </nav>
+  );
+}
