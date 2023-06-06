1b93c004413ab748062e910cbf561d45f86ce101
Andre Enzo <andreenzo479@gmail.com>
2023-06-06 15:50:01 +0200

    Restructuration branche

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import SignIn from "../components/SignIN";
-import Score from "../components/Score";
 import "../App.scss";
 
 export default function Home() {
   return (
     <div className="Home">
       <SignIn element={SignIn} />
-      <Score element={Score} />
     </div>
   );
 }
