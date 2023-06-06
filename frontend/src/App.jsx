13e0fb5b8ec49b438e0290ae286dcc055cbd0e9c
Andre Enzo <andreenzo479@gmail.com>
2023-06-06 15:49:02 +0200

    Restructuration branche

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,5 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
 import "./App.scss";
 import "./App.css";
 
@@ -11,11 +7,6 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-      </Routes>
     </div>
   );
 }
