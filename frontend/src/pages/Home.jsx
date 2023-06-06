d227621897b0c979178eb0da0cd9b00f208e99ab
Andre Enzo <andreenzo479@gmail.com>
2023-06-06 14:04:46 +0200

    changelent nom

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import SignIn from "../components/SignIN";
+import Score from "../components/Score";
 import "../App.scss";
-import Notation from "../components/Notation";
 
 export default function Home() {
   return (
     <div className="Home">
       <SignIn element={SignIn} />
-      <Notation element={Notation} />
+      <Score element={Score} />
     </div>
   );
 }
