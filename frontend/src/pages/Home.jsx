e79a826f8aebb7a67ce70f14bc401f71c696a08d
Andre Enzo <andreenzo479@gmail.com>
2023-06-07 15:27:24 +0200

    correction erreur apres Pr

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-import SignIn from "../components/SignIN";
+import SignIn from "../components/SignIn";
 import "../App.scss";
 
 export default function Home() {
   return (
     <div className="Home">
       <h1> "hello world depuis Home !!!" </h1>;
-      <SignIn element={SignIn} />
+      <SignIn />
     </div>
   );
 }
