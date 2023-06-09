e57faa7c0715df6731d75112db60fc56db631c07
Andre Enzo <andreenzo479@gmail.com>
2023-06-09 12:31:14 +0200

    Refait ma Pr apres supression de mon composant pendant les réglages conflits

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Profile() {
   return (
-    <div>
-      <h1>PAGE PROFILE</h1>
+    <div className="profil">
+      <h1>PROFIL</h1>
+      <h2>Informations du compte</h2>
+      <h2>Vos gouts</h2>
+      <Link to="/home">Retour</Link>
     </div>
   );
 }
