12271b99f49c77c5db7605d1866b6c9a8d197d91
Mohamed4900 <Mohamedibnlakhdar10@gmail.com>
2023-06-14 11:32:14 +0200

    update

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import SignIn from "../components/SignIn";
 
 export default function Home() {
   return (
     <div className="Home">
-      <h1>hello</h1>
+      <SignIn element={SignIn} />
     </div>
   );
 }
