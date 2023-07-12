f96c71a5640c974a4f38427fcdf75bc64b486ef5
Célia Kleczynki <Celia.kleczynski@hotmail.com>
2023-07-12 11:39:14 +0200

    Addition of the first name display with the Welcome by a fetch

--- frontend/src/pages/Intro.jsx
+++ frontend/src/pages/Intro.jsx
@@ -1,11 +1,38 @@
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 import wines from "../Pictures/WinesColor.png";
+import { useAuth } from "../contexts/AuthContext";
 
 export default function Intro() {
+  const [informations, setInformations] = useState([]);
+  const { token } = useAuth();
+
+  const fetchInformationsData = () => {
+    fetch(
+      `${import.meta.env.VITE_BACKEND_URL ?? "http://localhost:5000"}/user`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => setInformations([data]))
+      .catch((error) => {
+        console.error("Error fetching informations data:", error);
+      });
+  };
+
+  useEffect(() => {
+    fetchInformationsData();
+  }, []);
+
   return (
     <section className="backgroundIntro">
       <img src={wines} alt="Bouteille de vin" />
-      <h1>Bienvenue ___</h1>
+      {informations.map((user) => (
+        <h1>Bienvenue {user.firstname}</h1>
+      ))}
       <div className="linkDiv">
         <Link className="linkStart" to="/FourGlass">
           COMMENCEZ L'AVENTURE
